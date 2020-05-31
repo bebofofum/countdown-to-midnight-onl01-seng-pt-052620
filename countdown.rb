1214411c4297ad83d028a59549bc536994e61fbb
Brian Boehm <bri.boehm@gmail.com>
2020-05-31 18:49:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,14 @@ def countdown(n)
   
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+  
+  while n > 0
+  puts "#{n} SECOND(S)!"
+  n -= 1
+  sleep 1.second
+  end
+  
+  puts "HAPPY NEW YEAR!"
+end
